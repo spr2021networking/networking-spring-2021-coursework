c6cbb93b08c8a1d2dc0502876a67b604d763236f
Benjamin Cooper <benjamin.cooper@mymail.champlain.edu>
2021-02-02 12:57:21 -0500

    Server work

--- GPRO Net SDK/source/gpro-net-Console-Server/main-server.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Server/main-server.cpp
@@ -24,19 +24,91 @@
 
 #include "gpro-net/gpro-net.h"
 
-
-#include <stdio.h>
+#include <cstdio>
 #include <stdlib.h>
 #include <string.h>
-
+#include <iostream>
+#include <stdio.h>
 
 #include "RakNet/RakPeerInterface.h"
+#include "RakNet/MessageIdentifiers.h"
 
+
+#define MAX_CLIENTS 10
+#define SERVER_PORT 7777
 
 int main(int const argc, char const* const argv[])
 {
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+	RakNet::Packet* packet;
+
+	//printf("(C) or (S)erver?\n");
+	//scanf("%s", str);
+	//if ((str[0] == 'S') || (str[0] == 'S'))
+	//{
+	RakNet::SocketDescriptor sd(SERVER_PORT, 0);
+	peer->Startup(MAX_CLIENTS, &sd, 1);
+	isServer = true;
+	//}
 
 
-	printf("\n\n");
-	system("pause");
+	// TODO - Add code body here
+	if (isServer)
+	{
+		printf("Starting the server.\n");
+		// We need to let the server accept incoming connections from the clients
+		peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+	}
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				if (isServer) {
+					printf("A client has disconnected.\n");
+				}
+				else {
+					printf("We have been disconnected.\n");
+				}
+				break;
+			case ID_CONNECTION_LOST:
+				if (isServer) {
+					printf("A client lost the connection.\n");
+				}
+				else {
+					printf("Connection lost.\n");
+				}
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
+
+	return 0;
 }
