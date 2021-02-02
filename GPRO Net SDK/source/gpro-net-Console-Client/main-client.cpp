1b322e21619331b653f47c4f7a17a57a0341e8b5
Scott Dagen <scott.dagen@mymail.champlain.edu>
2021-02-02 11:59:54 -0500

    Making some progress on getting things to work

--- GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
@@ -24,19 +24,41 @@
 
 #include "gpro-net/gpro-net.h"
 
-
-#include <stdio.h>
+#include <cstdio>
 #include <stdlib.h>
 #include <string.h>
-
+#include <iostream>
+#include <stdio.h>
 
 #include "RakNet/RakPeerInterface.h"
 
+#define MAX_CLIENTS 10
+#define SERVER_PORT 60000
 
 int main(int const argc, char const* const argv[])
 {
+	char str[512];
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+
+	printf("(C) or (S)erver?\n");
+	scanf("%s", str);
+	if ((str[0] == 'c') || (str[0] == 'C'))
+	{
+		RakNet::SocketDescriptor sd;
+		peer->Startup(1, &sd, 1);
+		isServer = false;
+	}
+	else {
+		RakNet::SocketDescriptor sd(SERVER_PORT, 0);
+		peer->Startup(MAX_CLIENTS, &sd, 1);
+		isServer = true;
+	}
 
 
-	printf("\n\n");
-	system("pause");
+	// TODO - Add code body here
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
+
+	return 0;
 }
