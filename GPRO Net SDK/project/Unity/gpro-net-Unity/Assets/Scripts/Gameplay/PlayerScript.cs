515401130cd287c81f3f132b5e2059cdbd1aab49
Scott Dagen <scott.dagen@mymail.champlain.edu>
2021-03-17 18:01:17 -0400

    Added project data that we need

---
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/Gameplay/PlayerScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkClient c;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
