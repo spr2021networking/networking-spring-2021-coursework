259261dae96acc23209b1df7463d8e955bb3b046
Benjamin Cooper <benjamin.cooper@mymail.champlain.edu>
2021-03-20 16:02:45 -0400

    Added client

---
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/ShieldClient.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+#pragma warning disable CS0618
+public class ShieldClient : MonoBehaviour
+{
+    private const int MAX_CONNECTION = 100;
+
+    private int port = 5701;
+
+    private int hostID;
+    private int webHostID;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private int connectionID;
+
+    private float connectionTime;
+    private bool isStarted = false;
+    private bool isConnected = false;
+    private byte error;
+
+
+    public void Connect()
+    {
+
+        NetworkTransport.Init();
+        ConnectionConfig cc = new ConnectionConfig();
+
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+        unreliableChannel = cc.AddChannel(QosType.Unreliable);
+
+        HostTopology topo = new HostTopology(cc, MAX_CONNECTION);
+
+        hostID = NetworkTransport.AddHost(topo, 0);
+        connectionID = NetworkTransport.Connect(hostID, "127.0.0.1", port, 0, out error);
+
+        connectionTime = Time.time;
+        isConnected = true;
+    }
+
+    private void Update()
+    {
+        if (!isConnected)
+            return;
+
+        int recHostID;
+        int connectionID;
+        int channelID;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recData = NetworkTransport.Receive(out hostID, out connectionID, out channelID, recBuffer, bufferSize, out dataSize, out error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                break;
+            case NetworkEventType.DataEvent:
+                break;
+            case NetworkEventType.DisconnectEvent:
+                break;
+        }
+    }
+}
+
+#pragma warning restore CS0618
