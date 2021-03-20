adf6e29e635abd3d66d80e8ec0ee93d7d74de87f
Scott Dagen <scott.dagen@mymail.champlain.edu>
2021-03-20 15:40:18 -0400

    Renamed ShieldNetworkmanager to ShieldServer

--- GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/ShieldServer.cs
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/ShieldServer.cs
@@ -4,34 +4,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class ShieldNetworkManager : MonoBehaviour
+public class ShieldServer : MonoBehaviour
 {
 
+    private int reliableChannelID;
+    private int unreliableChannelID;
+
+    private int hostID;
+
+    private byte error;
+
+    private int port = 7777;
+
+    private bool running = false;
     // Start is called before the first frame update
     void Start()
     {
         //some init stuff that I don't yet understand
         NetworkTransport.Init();
-        GlobalConfig gConfig = new GlobalConfig();
-        gConfig.MaxPacketSize = 500;
-        NetworkTransport.Init(gConfig);
 
         //define the type of connections. Channels are basically data streams, probably used for different types of info
         ConnectionConfig config = new ConnectionConfig();
-        int myReliableChannelId = config.AddChannel(QosType.Reliable);
-        int myUnreliableChannelID = config.AddChannel(QosType.Unreliable);
+        reliableChannelID = config.AddChannel(QosType.Reliable);
+        unreliableChannelID = config.AddChannel(QosType.Unreliable);
 
         HostTopology topology = new HostTopology(config, 10); //set to 1 for client, 10 for server. Use the ConnectionConfig from before
 
         //finish making the IP
-        int hostId = NetworkTransport.AddHost(topology, 0);
-        Debug.Log(hostId);
-        byte error;
-        int connectionId = NetworkTransport.Connect(hostId, "192.168.1.42", 8888, 0, out error);
-        //
-        //byte[] buffer = new byte[16];
-        //int bufferLength = 16;
-        //NetworkTransport.Send(hostId, connectionId, myReliableChannelId, buffer, bufferLength, out error);
+        hostID = NetworkTransport.AddHost(topology, 0);
+
+        running = true;
     }
 
     // Update is called once per frame
