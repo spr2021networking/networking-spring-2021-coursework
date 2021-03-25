2d7907f099a2ccd62443211e50a79076b7dd6880
Benjamin Cooper <benjamin.cooper@mymail.champlain.edu>
2021-03-25 19:04:51 -0400

    Added player input

--- GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/PlayerInput.cs
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/PlayerInput.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    Rigidbody m_Rigidbody;
+    [SerializeField]
+    float movementSpeed = 2.5f;
+
+    float newXPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        m_Rigidbody.MovePosition(transform.position + m_Input * Time.deltaTime * movementSpeed);
     }
+
 }
